e073a2c921cfe8b21971458c928859337099f23d
aeddi <antoine.e.b@gmail.com>
2016-10-25 14:13:13 +0200

    Colleen done

---
+++ Colleen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Com1
+void print_src(char *src, char *spe)
+{
+	printf(src, spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[1], spe[3], spe[2], spe[2], spe[2], spe[2], spe[2], spe[3], spe[3], spe[0], spe[1], spe[3], src, spe[3], spe[0], spe[0], spe[1], spe[0], spe[1], spe[0], spe[0]);
+}
+
+int main(void)
+{
+	// Com2
+	char spe[] = "\n\t\\\"";
+	char *src = "#include <stdio.h>%c%c// Com1%cvoid print_src(char *src, char *spe)%c{%c%cprintf(src, spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[1], spe[3], spe[2], spe[2], spe[2], spe[2], spe[2], spe[3], spe[3], spe[0], spe[1], spe[3], src, spe[3], spe[0], spe[0], spe[1], spe[0], spe[1], spe[0], spe[0]);%c}%c%cint main(void)%c{%c%c// Com2%c%cchar spe[] = %c%cn%ct%c%c%c%c%c;%c%cchar *src = %c%s%c;%c%c%cprint_src(src, spe);%c%creturn 0;%c}%c";
+
+	print_src(src, spe);
+	return 0;
+}
