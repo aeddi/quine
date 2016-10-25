fd2ab6f50501abf160ce8964124b110d088beea7
aeddi <antoine.e.b@gmail.com>
2016-10-25 16:04:58 +0200

    Edit maccro Grace

--- Grace.c
+++ Grace.c
@@ -2,15 +2,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define obfs(m,a,i,n) m##a##i##n
-#define begin obfs(m,a,i,n)
-#define start int begin() {print_src(); return 0;}
+#define OBFS(m,a,i,n) m##a##i##n
+#define BEGIN OBFS(m,a,i,n)
+#define START(x) int BEGIN() {print_src(); return x;}
 
 void print_src()
 {
 	// Com
 	char spe[] = "\n\t\\\"";
-	char *src = "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c#define obfs(m,a,i,n) m##a##i##n%c#define begin obfs(m,a,i,n)%c#define start int begin() {print_src(); return 0;}%c%cvoid print_src()%c{%c%c// Com%c%cchar spe[] = %c%cn%ct%c%c%c%c%c;%c%cchar *src = %c%s%c;%c%cint fd = open(%c./Grace_kid.c%c, O_RDWR|O_CREAT);%c%c%cif (fd != -1) {%c%c%cdprintf(fd, src, spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[1], spe[3], spe[2], spe[2], spe[2], spe[2], spe[2], spe[3], spe[3], spe[0], spe[1], spe[3], src, spe[3], spe[0], spe[1], spe[3], spe[3], spe[0], spe[0], spe[1], spe[0], spe[1], spe[1], spe[0], spe[1], spe[1], spe[0], spe[1], spe[0], spe[0], spe[0], spe[0]);%c%c%cclose(fd);%c%c}%c}%c%cstart%c";
+	char *src = "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c#define OBFS(m,a,i,n) m##a##i##n%c#define BEGIN OBFS(m,a,i,n)%c#define START(x) int BEGIN() {print_src(); return x;}%c%cvoid print_src()%c{%c%c// Com%c%cchar spe[] = %c%cn%ct%c%c%c%c%c;%c%cchar *src = %c%s%c;%c%cint fd = open(%c./Grace_kid.c%c, O_RDWR|O_CREAT);%c%c%cif (fd != -1) {%c%c%cdprintf(fd, src, spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[1], spe[3], spe[2], spe[2], spe[2], spe[2], spe[2], spe[3], spe[3], spe[0], spe[1], spe[3], src, spe[3], spe[0], spe[1], spe[3], spe[3], spe[0], spe[0], spe[1], spe[0], spe[1], spe[1], spe[0], spe[1], spe[1], spe[0], spe[1], spe[0], spe[0], spe[0], spe[0]);%c%c%cclose(fd);%c%c}%c}%c%cSTART(0)%c";
 	int fd = open("./Grace_kid.c", O_RDWR|O_CREAT);
 
 	if (fd != -1) {
@@ -19,4 +19,4 @@ void print_src()
 	}
 }
 
-start
+START(0)
