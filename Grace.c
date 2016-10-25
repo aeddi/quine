89fdace64454bf0e4fb4a049f83796555ceb24a9
aeddi <antoine.e.b@gmail.com>
2016-10-25 15:48:06 +0200

    Fix error management on Grace

--- Grace.c
+++ Grace.c
@@ -10,11 +10,13 @@ void print_src()
 {
 	// Com
 	char spe[] = "\n\t\\\"";
-	char *src = "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c#define obfs(m,a,i,n) m##a##i##n%c#define begin obfs(m,a,i,n)%c#define start int begin() {print_src(); return 0;}%c%cvoid print_src()%c{%c%c// Com%c%cchar spe[] = %c%cn%ct%c%c%c%c%c;%c%cchar *src = %c%s%c;%c%cint fd = open(%c./Grace_kid.c%c, O_RDWR|O_CREAT);%c%c%cdprintf(fd, src, spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[1], spe[3], spe[2], spe[2], spe[2], spe[2], spe[2], spe[3], spe[3], spe[0], spe[1], spe[3], src, spe[3], spe[0], spe[1], spe[3], spe[3], spe[0], spe[0], spe[1], spe[0], spe[1], spe[0], spe[0], spe[0], spe[0]);%c%cclose(fd);%c}%c%cstart%c";
+	char *src = "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c#define obfs(m,a,i,n) m##a##i##n%c#define begin obfs(m,a,i,n)%c#define start int begin() {print_src(); return 0;}%c%cvoid print_src()%c{%c%c// Com%c%cchar spe[] = %c%cn%ct%c%c%c%c%c;%c%cchar *src = %c%s%c;%c%cint fd = open(%c./Grace_kid.c%c, O_RDWR|O_CREAT);%c%c%cif (fd != -1) {%c%c%cdprintf(fd, src, spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[1], spe[3], spe[2], spe[2], spe[2], spe[2], spe[2], spe[3], spe[3], spe[0], spe[1], spe[3], src, spe[3], spe[0], spe[1], spe[3], spe[3], spe[0], spe[0], spe[1], spe[0], spe[1], spe[1], spe[0], spe[1], spe[1], spe[0], spe[1], spe[0], spe[0], spe[0], spe[0]);%c%c%cclose(fd);%c%c}%c}%c%cstart%c";
 	int fd = open("./Grace_kid.c", O_RDWR|O_CREAT);
 
-	dprintf(fd, src, spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[1], spe[3], spe[2], spe[2], spe[2], spe[2], spe[2], spe[3], spe[3], spe[0], spe[1], spe[3], src, spe[3], spe[0], spe[1], spe[3], spe[3], spe[0], spe[0], spe[1], spe[0], spe[1], spe[0], spe[0], spe[0], spe[0]);
-	close(fd);
+	if (fd != -1) {
+		dprintf(fd, src, spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[0], spe[1], spe[0], spe[1], spe[3], spe[2], spe[2], spe[2], spe[2], spe[2], spe[3], spe[3], spe[0], spe[1], spe[3], src, spe[3], spe[0], spe[1], spe[3], spe[3], spe[0], spe[0], spe[1], spe[0], spe[1], spe[1], spe[0], spe[1], spe[1], spe[0], spe[1], spe[0], spe[0], spe[0], spe[0]);
+		close(fd);
+	}
 }
 
 start
